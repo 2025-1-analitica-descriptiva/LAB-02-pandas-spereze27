f8e9c6fe5d68b991a1fdbf944138ad9e20c0bbea
simonQuind <simon.perez@quind.io>
2025-06-06 17:43:23 -0500

    pregunta 9

--- homework/pregunta_09.py
+++ homework/pregunta_09.py
@@ -6,20 +6,33 @@ librerias de pandas para resolver las preguntas.
 """
 
 
+import pandas as pd
+
 def pregunta_09():
-    """
-    Agregue el año como una columna al dataframe que contiene el archivo
-    `tbl0.tsv`.
+    df = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+    
+    # Corrige fecha inválida manualmente
+    df["c3"] = df["c3"].replace("1999-02-29", "1999-02-28")
+    
+    # Conversión a datetime
+    df["year"] = pd.to_datetime(df["c3"]).dt.year.astype(str) 
+    df["c3"] = df["c3"].replace("1999-02-28", "1999-02-29")
+    return df
 
-    Rta/
-        c0 c1  c2          c3  year
-    0    0  E   1  1999-02-28  1999
-    1    1  A   2  1999-10-28  1999
-    2    2  B   5  1998-05-02  1998
-    ...
-    36  36  B   8  1997-05-21  1997
-    37  37  C   9  1997-07-22  1997
-    38  38  E   1  1999-09-28  1999
-    39  39  E   5  1998-01-26  1998
+print(pregunta_09())
+"""
+Agregue el año como una columna al dataframe que contiene el archivo
+`tbl0.tsv`.
 
-    """
+Rta/
+    c0 c1  c2          c3  year
+0    0  E   1  1999-02-28  1999
+1    1  A   2  1999-10-28  1999
+2    2  B   5  1998-05-02  1998
+...
+36  36  B   8  1997-05-21  1997
+37  37  C   9  1997-07-22  1997
+38  38  E   1  1999-09-28  1999
+39  39  E   5  1998-01-26  1998
+
+"""
