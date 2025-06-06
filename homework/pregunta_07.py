903347a20a165f05253b028366803587f104a98b
simonQuind <simon.perez@quind.io>
2025-06-06 15:57:43 -0500

    pregunta 7

--- homework/pregunta_07.py
+++ homework/pregunta_07.py
@@ -6,17 +6,23 @@ librerias de pandas para resolver las preguntas.
 """
 
 
-def pregunta_07():
-    """
-    Calcule la suma de la `c2` por cada letra de la `c1` del archivo
-    `tbl0.tsv`.
+import pandas as pd
 
-    Rta/
-    c1
-    A    37
-    B    36
-    C    27
-    D    23
-    E    67
-    Name: c2, dtype: int64
-    """
+def pregunta_07():
+    df = pd.read_csv("files/input/tbl0.tsv", sep="\t")
+    suma = df.groupby("c1")["c2"].sum()
+    return suma
+print(pregunta_07())
+"""
+Calcule la suma de la `c2` por cada letra de la `c1` del archivo
+`tbl0.tsv`.
+
+Rta/
+c1
+A    37
+B    36
+C    27
+D    23
+E    67
+Name: c2, dtype: int64
+"""
